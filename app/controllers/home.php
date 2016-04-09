b8d2b4635b5ea90193f95f4971e93f41a275b6ad
pritam1605 <pritamkumar@ebanx.com>
2016-04-09 19:55:45 -0300

    Adds the ORM (Eloquent) functionality

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -2,12 +2,24 @@
 
 	class Home extends Controller {
 
-		public function index($param) {
+		protected $user;
+		public function index($param = '') {
 
 			$user = $this->_model('user');
 			$user->a = $param;
 
 			$this->_view('home/main', ['title' => 'User', 'name' => $user->a]);
+
+		}
+
+		public function create($username = '', $password = '', $name ='', $surname = '') {
+
+			User::create([
+				'username' => $username,
+				'password' => $password,
+				'first_name' => $name,
+				'last_name' => $surname,
+			]);
 
 		}
 	}
